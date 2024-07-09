8e5e679f7955375ef0ec3535e465773237a9f5e7
Almaz Ilaletdinov <a.ilaletdinov@yandex.ru>
2024-07-09 14:31:15 +0300

    Replace diff

--- tests/it/test_main.py
+++ tests/it/test_main.py
@@ -30,34 +30,13 @@ from ondivi.entry import controller
 def test_controller() -> None:
     """Testing script output with diff and violations list."""
     got, found = controller(
-        Path('tests/fixtures/diff.txt').read_text(encoding='utf-8'),
+        Path('tests/fixtures/diff.patch').read_text(encoding='utf-8'),
         Path('tests/fixtures/violations.txt').read_text(encoding='utf-8').splitlines(),
         '{filename}:{line_num:d}:{col_num:d}: {message}',
         only_violations=False,
     )
 
     assert got == [
-        'src/app_types/listable.py:23:1: UP035 Import from `collections.abc` instead: `Sequence`',
-        'src/srv/ayats/ayats_by_text_query.py:23:1: UP035 Import from `collections.abc` instead: `Sequence`',
-        'src/srv/ayats/ayats_by_text_query.py:23:47: F401 [*] `typing.Generic` imported but unused',
-        'src/srv/ayats/favorite_ayats_after_remove.py:23:1: UP035 Import from `collections.abc` instead: `Sequence`',
-        ' '.join([
-            'src/srv/ayats/pg_ayat.py:64:30: PLR2004 Magic value used in comparison,',
-            'consider replacing `4096` with a constant variable',
-        ]),
-        'src/srv/ayats/pg_ayat.py:66:44: COM812 Trailing comma missing',
-        ' '.join([
-            "src/tests/it/srv/ayats/test_pg_ayat.py:78:19: PLC1901 `got == ''` can be simplified",
-            'to `not got` as an empty string is falsey',
-        ]),
-        'src/app_types/listable.py:33:20: PYI059 `Generic[]` should always be the last base class',
-        ' '.join([
-            'src/tests/it/srv/ayats/test_pg_ayat.py:90:13: PLW1514 `pathlib.Path(...).read_text`',
-            'without explicit `encoding` argument',
-        ]),
-        ' '.join([
-            'src/tests/it/srv/ayats/test_pg_ayat.py:95:19: PLW1514 `pathlib.Path(...).read_text`',
-            'without explicit `encoding` argument',
-        ]),
+        'django/db/models/sql/query.py:1404:9: ANN201 Missing return type annotation for public function `try_transform`', 'django/db/models/sql/query.py:1404:29: ANN001 Missing type annotation for function argument `lhs`', 'django/db/models/sql/query.py:1404:34: ANN001 Missing type annotation for function argument `name`', 'django/db/models/sql/query.py:1404:40: ANN001 Missing type annotation for function argument `lookups`', 'django/db/models/sql/query.py:1405:9: D205 1 blank line required between summary line and description', 'django/db/models/sql/query.py:1405:9: D212 [*] Multi-line docstring summary should start at the first line', 'django/db/models/sql/query.py:1405:9: D401 First line of docstring should be in imperative mood: "Helper method for build_lookup(). Try to fetch and initialize"', 'django/db/models/sql/query.py:1418:30: UP031 Use format specifiers instead of percent format', 'django/db/models/sql/query.py:1427:17: UP031 Use format specifiers instead of percent format', 'django/db/models/sql/query.py:1428:88: COM812 [*] Trailing comma missing', 'django/db/models/sql/query.py:1431:9: C901 `build_filter` is too complex (20 > 10)', 'django/db/models/sql/query.py:1431:9: PLR0913 Too many arguments in function definition (10 > 5)', 'django/db/models/sql/query.py:1431:9: PLR0912 Too many branches (21 > 12)', 'django/db/models/sql/query.py:1431:9: PLR0915 Too many statements (60 > 50)', 'django/db/models/sql/query.py:1431:9: ANN201 Missing return type annotation for public function `build_filter`', 'tests/custom_lookups/tests.py:623:13: PT009 Use a regular `assert` instead of unittest-style `assertEqual`', 'tests/lookup/tests.py:843:9: ANN201 Missing return type annotation for public function `test_unsupported_lookups_custom_lookups`', 'tests/lookup/tests.py:843:9: D102 Missing docstring in public method', 'tests/lookup/tests.py:844:22: SLF001 Private member accessed: `_meta`', 'tests/lookup/tests.py:853:9: ANN201 Missing return type annotation for public function `test_relation_nested_lookup_error`', 'tests/lookup/tests.py:853:9: D102 Missing docstring in public method',
     ]
     assert found
--- tests/unit/test_define_changed_lines.py
+++ tests/unit/test_define_changed_lines.py
@@ -30,23 +30,23 @@ from ondivi.define_changed_lines import define_changed_lines
 def test_define_changed_files() -> None:
     """Testing search changed lines."""
     got = define_changed_lines(
-        Path('tests/fixtures/diff.txt').read_text(encoding='utf-8'),
+        Path('tests/fixtures/diff.patch').read_text(encoding='utf-8'),
     )
 
     assert got == {
-        'src/app_types/listable.py': [23, 29, 33, 36, 37, 44, 47, 49],
-        'src/srv/ayats/ayat_by_id_answer.py': [36, 58],
-        'src/srv/ayats/ayat_by_sura_ayat_num_answer.py': [38, 59],
-        'src/srv/ayats/ayats_by_text_query.py': [23, 33, 34, 47, 62, 63, 64, 65, 66],
-        'src/srv/ayats/change_favorite_ayat_answer.py': [55, 77, 78, 79, 80, 81, 82, 84],
-        'src/srv/ayats/favorite_ayats.py': [33, 60],
-        'src/srv/ayats/favorite_ayats_after_remove.py': [23, 34, 48, 67, 69],
-        'src/srv/ayats/favorites/user_favorite_ayats.py': [32, 60],
-        'src/srv/ayats/neighbor_ayats.py': [33, 137, 154, 197, 198, 199, 213, 214, 215],
-        'src/srv/ayats/pg_ayat.py': [28, 36, *range(44, 91), 100],
-        'src/tests/fixtures/2_282_ayat_rendered.txt': [1, 2, 3, 4, 5, 6],
-        'src/tests/fixtures/2_282_ayat_without_transliteration.txt': [1, 2, 3, 4],
-        'src/tests/it/srv/ayats/test_pg_ayat.py': [24, 30, 31, 32, 33, 34, *range(74, 98)],
+        'django/db/models/sql/query.py': [
+            *range(1367, 1374),
+            *range(1401, 1408),
+            *range(1418, 1432),
+        ],
+        'tests/custom_lookups/tests.py': [
+            *range(614, 624),
+        ],
+        'tests/lookup/tests.py': [
+            *range(812, 846),
+            *range(853, 860),
+            *range(1087, 1097),
+        ],
     }
 
 

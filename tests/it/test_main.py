9723db1ec9a8e9754809c533d48e7ea481b35cfb
Айгуль Зангирова <a.ilaletdinov@yandex.ru>
2025-02-17 17:15:08 +0300

    Fix test_controller.py

--- tests/it/test_main.py
+++ tests/it/test_main.py
@@ -22,19 +22,35 @@
 
 """Tests for ondivi."""
 
+import pytest
 from pathlib import Path
 
 from ondivi.entry import controller
 
 
-def test_controller() -> None:
+@pytest.fixture
+def localize_violation_path():
+    def _localize_violation_path(violation: str):
+        parts = violation.split(':')
+        parts[0] = str(Path(parts[0]))
+        return ':'.join(parts)
+    return _localize_violation_path
+
+
+def test_controller(localize_violation_path) -> None:
     """Testing script output with diff and violations list."""
     got, found = controller(
         Path('tests/fixtures/diff.patch').read_text(encoding='utf-8'),
-        Path('tests/fixtures/violations.txt').read_text(encoding='utf-8').splitlines(),
+        [
+            localize_violation_path(line)
+            for line in Path('tests/fixtures/violations.txt').read_text(encoding='utf-8').splitlines()
+        ],
         '{filename}:{line_num:d}:{col_num:d}: {message}',
         only_violations=False,
     )
 
-    assert got == Path('tests/fixtures/actual_violations.txt').read_text(encoding='utf-8').splitlines()
+    assert got == [
+        localize_violation_path(line)
+        for line in Path('tests/fixtures/actual_violations.txt').read_text(encoding='utf-8').splitlines()
+    ]
     assert found

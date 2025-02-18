586b5bf9df0df8e5e557154679a79db3c5a9b242
Almaz Ilaletdinov <a.ilaletdinov@yandex.ru>
2025-02-18 11:43:09 +0300

    fix tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -20,42 +20,22 @@
 # OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE
 # OR OTHER DEALINGS IN THE SOFTWARE.
 
-"""Tests for ondivi."""
+"""Fixtures."""
 
 from pathlib import Path
 from typing import Callable
 
-from ondivi._internal.define_changed_lines import define_changed_lines  # noqa: WPS436. _internal allow into ondivi app
+import pytest
 
 
-def test_define_changed_files(localize_violation_path: Callable[[str], str]) -> None:
-    """Testing search changed lines."""
-    got = define_changed_lines(
-        Path('tests/fixtures/diff.patch').read_text(encoding='utf-8'),
-    )
+@pytest.fixture
+def localize_violation_path() -> Callable[[str], str]:
+    """Localize violation path.
 
-    assert got == {
-        localize_violation_path('django/db/models/sql/query.py'): [
-            *range(1367, 1374),
-            *range(1401, 1408),
-            *range(1418, 1432),
-        ],
-        localize_violation_path('tests/custom_lookups/tests.py'): list(range(614, 624)),
-        localize_violation_path('tests/lookup/tests.py'): [
-            *range(812, 846),
-            *range(853, 860),
-            *range(1087, 1097),
-        ],
-    }
-
-
-def test_define_filename() -> None:
-    """Test define filename.
-
-    Created for kill mutant
+    Violation files contain strings with unix paths, but we need to convert them to windows paths.
     """
-    got = define_changed_lines(
-        'diff --git XX b/ XX b/ file.py',
-    )
-
-    assert got == {'file.py': []}
+    def _localize_violation_path(violation: str) -> str:  # noqa: WPS430
+        parts = violation.split(':')
+        parts[0] = str(Path(parts[0]))
+        return ':'.join(parts)
+    return _localize_violation_path

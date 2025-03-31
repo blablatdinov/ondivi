1baf3be8e4f9c4dc0fa3ad377f8f078e08f52200
Almaz Ilaletdinov <a.ilaletdinov@yandex.ru>
2025-03-31 20:43:54 +0300

    noqa comment

--- tests/it/test_app.py
+++ tests/it/test_app.py
@@ -20,6 +20,8 @@
 # OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE
 # OR OTHER DEALINGS IN THE SOFTWARE.
 
+# flake8: noqa: WPS202
+
 """Integration test with installing and check on real git repo."""
 
 import os

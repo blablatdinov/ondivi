c6dff1f464481b1559710a9e27fe60992f825c70
Almaz Ilaletdinov <a.ilaletdinov@yandex.ru>
2025-12-29 15:28:36 +0300

    Update license year

--- ondivi/entry.py
+++ ondivi/entry.py
@@ -1,24 +1,5 @@
-# The MIT License (MIT).
-#
-# Copyright (c) 2024-2025 Almaz Ilaletdinov <a.ilaletdinov@yandex.ru>
-#
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-#
-# The above copyright notice and this permission notice shall be included in all
-# copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
-# IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
-# DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
-# OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE
-# OR OTHER DEALINGS IN THE SOFTWARE.
+# SPDX-FileCopyrightText: Copyright (c) 2024-2026 Almaz Ilaletdinov <a.ilaletdinov@yandex.ru>
+# SPDX-License-Identifier: MIT
 
 """Ondivi (Only diff violations).
 
@@ -97,7 +78,7 @@ def cli(
         else sys.stdin.read().strip().splitlines()
     )
     try:
-        diff = Repo('.').git.diff('--unified=0', '--no-ext-diff', '--src-prefix=a/', '--dst-prefix=b/', baseline)
+        diff = Repo('.').git.diff('--unified=0', '--no-ext-diff', baseline)
     except GitCommandError:
         sys.stdout.write('Revision "{0}" not found'.format(baseline))
         sys.exit(1)

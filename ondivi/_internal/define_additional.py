86292ff1138adfb75a607ad08680790db067b04d
Almaz Ilaletdinov <a.ilaletdinov@yandex.ru>
2026-05-15 12:31:14 +0300

    Sorting not actual

--- ondivi/_internal/define_additional.py
+++ ondivi/_internal/define_additional.py
@@ -7,10 +7,6 @@ from random import Random
 
 
 def define_additional(linter_output: list[str], filtered_lines: list[str], size: int) -> list[str]:
-    not_actual = list(set(linter_output) - set(filtered_lines))
-    hsh = hashlib.md5(''.join(sorted(not_actual)).encode('utf-8')).digest()[:4]
-    print('linter_output =', pformat(linter_output, width=200))
-    print('not actual =', pformat(not_actual, width=200))
-    print('hash =', hsh)
-    print('random violations =', pformat(sorted(Random(hsh).sample(not_actual, k=size)), width=200))
+    not_actual = sorted(list(set(linter_output) - set(filtered_lines)))
+    hsh = hashlib.md5(''.join(not_actual).encode('utf-8')).digest()[:4]
     return sorted(Random(hsh).sample(not_actual, k=size))

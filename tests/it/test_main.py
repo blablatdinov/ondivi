c6fcc356a541e6b7a7a2841896bbdc4c69ae03be
Almaz Ilaletdinov <a.ilaletdinov@yandex.ru>
2026-05-15 15:37:20 +0300

    remove unused tests

--- tests/it/test_main.py
+++ tests/it/test_main.py
@@ -27,20 +27,3 @@ def test_controller(localize_violation_path: Callable[[str], str]) -> None:
         for line in Path('tests/fixtures/actual_violations.txt').read_text(encoding='utf-8').splitlines()
     ]
     assert found
-
-
-def test_additional_violations(localize_violation_path: Callable[[str], str]) -> None:
-    """Testing additional violations."""
-    got, found = controller(
-        Path('tests/fixtures/diff.patch').read_text(encoding='utf-8'),
-        [
-            localize_violation_path(line)
-            for line in Path('tests/fixtures/violations.txt').read_text(encoding='utf-8').splitlines()
-        ],
-        '{filename}:{line_num:d}:{col_num:d}: {message}',
-        only_violations=False,
-        random_additional='5',
-    )
-
-    assert len(got) == 26
-    assert found
